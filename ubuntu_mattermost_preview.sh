94a66fc81a80a3c6d9b1406ee62231f6f426910f
fxixf <35148498+fxixf@users.noreply.github.com>
2018-07-13 09:57:38 -0700

    basic script for preiew

---
+++ ubuntu_mattermost_preview.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install wget
+wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker ubuntu
+sudo service docker start
+newgrp docker
+docker run --name mattermost-preview -d --publish 8065:8065 --add-host dockerhost:127.0.0.1 mattermost/mattermost-preview
