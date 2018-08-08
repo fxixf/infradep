6e6fea121a709f01124b2e63c14813838e1b387a
Chris Lin <chris.h.lin@gmail.com>
2018-08-08 00:00:05 -0700

    ubuntu_prep

--- config/mattermost/mattermost_prod.sh
+++ config/mattermost/mattermost_prod.sh
@@ -4,11 +4,22 @@ wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker ubuntu
 sudo service docker start
 newgrp docker
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+// curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/mattermost/mattermost-docker.git
-cd mattermost-docker
+
+// git clone https://github.com/mattermost/mattermost-docker.git
+// cd mattermost-docker
+// mkdir -pv ./volumes/app/mattermost/{data,logs,config}
+
+//tar -zxvf infrafx-mm-compose.tar.gz
+sudo tar -zxvf infrafx-mm-compose.tar.gz
+cd infrafx-mattermost-compose/
+
+
 docker-compose build
-mkdir -pv ./volumes/app/mattermost/{data,logs,config}
-chown -R 2000:2000 ./volumes/app/mattermost/
+
+//chown -R 2000:2000 ./volumes/app/mattermost/
+sudo chown -R 2000:2000 ./volumes/app/mattermost/
 docker-compose up -d
